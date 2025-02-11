e115407f39d4e17efa21ffa0e45803199dec956c
Akobir Musaev <akobirmusaev@Akobirs-MacBook-Pro.local>
2025-02-11 18:35:36 +0900

    TheLastCommit

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,23 +1,38 @@
 "use client";
 
-import Loader from "@/components/Loader";
-import MeetingRoom from "@/components/MeetingRoom";
-import MeetingSetup from "@/components/MeetingSetup";
-import { useGetCallById } from "@/hooks/useGetCallById";
+import { useState } from "react";
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
-import React, { useState, use } from "react";
+import { useParams } from "next/navigation";
+import { Loader } from "lucide-react";
 
-const Meeting = ({ params }: { params: Promise<{ id: string }> }) => {
-  const { user, isLoaded } = useUser();
+import { useGetCallById } from "@/hooks/useGetCallById";
+import Alert from "@/components/Alert";
+import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
+
+const MeetingPage = () => {
+  const { id } = useParams();
+  const { isLoaded, user } = useUser();
+  const { call, isCallLoading } = useGetCallById(id!);
   const [isSetupComplete, setIsSetupComplete] = useState(false);
 
-  // ✅ Unwrap params using use()
-  const { id } = use(params);
-
-  const { call, isCallLoading } = useGetCallById(id);
-
   if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call)
+    return (
+      <p className="text-center text-3xl font-bold text-white">
+        Call Not Found
+      </p>
+    );
+
+  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+  const notAllowed =
+    call.type === "invited" &&
+    (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowed)
+    return <Alert title="You are not allowed to join this meeting" />;
 
   return (
     <main className="h-screen w-full">
@@ -34,4 +49,4 @@ const Meeting = ({ params }: { params: Promise<{ id: string }> }) => {
   );
 };
 
-export default Meeting;
+export default MeetingPage;
